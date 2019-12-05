e268786e734f457d0b3225de159cde689ea8d452
Krisztián Ádám <adamkrisz@protonmail.com>
2019-12-05 22:41:43 +0200

    make graph curvy

--- apps/fungifarm_web/lib/fungifarm_web/views/graph.ex
+++ apps/fungifarm_web/lib/fungifarm_web/views/graph.ex
@@ -1,17 +1,4 @@
 defmodule FungifarmWeb.Graph do
-  use Phoenix.LiveView
-
-# warning: function render/1 required by behaviour Phoenix.LiveView is not implemented (in module FungifarmWeb.Graph)
-#   lib/fungifarm_web/views/graph.ex:1: FungifarmWeb.Graph (module)
-# defmodule Asd do
-#   def read() do
-#     receive do
-#       a -> IO.inspect(a)
-#     end
-#   end
-# end
-
-
   def data_to_points([], _width, _height), do: ""
 
   def data_to_points(data, width, _height) do
@@ -24,5 +11,40 @@ defmodule FungifarmWeb.Graph do
     |> Enum.zip(data)
     |> Enum.flat_map(fn {x, y} -> [x, y] end)
     |> Enum.join(",")
+  end
+
+  def data_to_path(data, _width, _height) when length(data) < 2, do: ""
+
+  def data_to_path(data, width, height) do
+    point_count = length(data)
+    step = width / (point_count - 1)
+
+    x_axis = Enum.map(0..point_count, &(&1 * step)) # point on the x-axis to use
+
+    data = Enum.zip(x_axis, data)
+
+    [first] = Enum.take(data, 1)
+    [last] = Enum.take(data, -1)
+
+    data = data |> Enum.drop(1) |> Enum.drop(-1)
+
+    # the first point is special
+    {x0, y0} = first
+    r_begin = "M #{x0} #{y0} C #{x0 + step / 2} #{y0}"
+
+    # C works like:
+    # <control point to left curve> <point> <control point for right curve>
+    r = Enum.map(data, fn {x, y} ->
+      " #{x - step / 2} #{y} #{x} #{y} #{x + step / 2} #{y}"
+    end) |> Enum.join(",")
+
+    # the last one doesnt have right curve
+    {xz, yz} = last
+    r_end = " #{xz - step / 2} #{yz} #{xz} #{yz} #{xz} #{yz}"
+
+    # make an angular bottom
+    r_bottom = " #{width} #{height} #{width} #{height} #{width} #{height} 0 #{height} 0 #{height} 0 #{height}"
+
+    r_begin <> r <> r_end <> r_bottom
   end
 end
