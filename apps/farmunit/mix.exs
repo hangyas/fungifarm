14473a9fa611c9bafe5f2e6e0ecb4fc3e9075774
Krisztián Ádám <adamkrisz@protonmail.com>
2020-01-23 18:47:25 +0100

    add CommandSensor

--- apps/farmunit/mix.exs
+++ apps/farmunit/mix.exs
@@ -27,6 +27,7 @@ defmodule FarmUnit.MixProject do
   defp deps do
     [
       {:pubsub, "~> 1.0"},
+      {:json, "~> 1.3"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:fungifarm_shared, in_umbrella: true}
