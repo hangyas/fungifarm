1735bce74cc61475544edcc01fc13a8154231e5d
Krisztián Ádám <adamkrisz@gmail.com>
2020-04-14 10:08:08 +0200

    add PulletMQ
    Home grown messageing queue, that can queue messages on the farm unit while the server is unavailable

---
+++ apps/fungifarm_shared/lib/pulletmq.ex
@@ -0,0 +1,120 @@
+defmodule PulletMQ do
+  @doc ~S"""
+  Minimalistic, persistent, deliver at-most-once message queue for durable network communicaiton
+
+  Messages are queued on the sender, the receiver requests the first element and receives it
+  as soon as it's available
+  """
+
+  use GenServer
+
+  @initial_state %{
+    config: %{},
+    cub: nil,
+    requests: []
+  }
+
+  @doc ~S"""
+  Required options:
+   - queue_id
+   - data_dir
+  """
+  def start_link(opts) do
+    state =
+      @initial_state
+      |> Map.put(:config, Map.new(opts))
+
+    GenServer.start_link(__MODULE__, state, name: opts[:queue_id])
+  end
+
+  # -- frontend --
+
+  @doc ~S"""
+  push item to the end of the queue
+  """
+  def push(pullet, item) do
+    GenServer.call(pullet, {:push, item})
+  end
+
+  @doc ~S"""
+  request the first item from the queue
+  the item will be sent to the destionation process as {:item, item} and removed from the queue
+  """
+  def request(pullet, destionation \\ self()) do
+    GenServer.call(pullet, {:request, destionation})
+  end
+
+  def stop(pullet, reason \\ :normal, timeout \\ :infinity) do
+    GenServer.stop(pullet, reason, timeout)
+  end
+
+  # -- backend --
+
+  def init(state) do
+    # if any of these dies, the full queue will die, which is fine for now
+    {:ok, cub_db} = CubDB.start_link(data_dir: state.config.data_dir)
+    {:ok, cub_q} = CubQ.start_link(db: cub_db, queue: state.config.queue_id)
+
+    state =
+      state
+      |> Map.put(:cub_db, cub_db)
+      |> Map.put(:queue, cub_q)
+
+    {:ok, state}
+  end
+
+  def handle_call({:push, item}, _from, state) do
+    with {req, requests_tail} <- pop_request(state.requests) do
+      answer_request(req, item)
+
+      state =
+        state
+        |> Map.put(:requests, requests_tail)
+
+      {:reply, :ok, state}
+    else
+      nil ->
+        :ok = CubQ.enqueue(state.queue, item)
+
+        {:reply, :ok, state}
+    end
+  end
+
+  def handle_call({:request, destionation}, _from, state) do
+    with {:ok, item} <- CubQ.dequeue(state.queue) do
+      # send immeditaly
+      answer_request(destionation, item)
+      {:reply, :ok, state}
+    else
+      nil ->
+        # subscribe for push event
+        state =
+          state
+          |> Map.put(:requests, state.requests ++ [destionation])
+
+        {:reply, :ok, state}
+    end
+  end
+
+  # -- private --
+
+  # pops the first request which is stil alive
+  # returns as {request, requests_tail}
+  # TODO maybe move this into a state struct
+
+  defp pop_request([]), do: nil
+
+  defp pop_request(requests) do
+    [req | tail] = requests
+
+    if Process.alive?(req) do
+      {req, tail}
+    else
+      pop_request(tail)
+    end
+  end
+
+  defp answer_request(destionation, item) do
+    send(destionation, {:item, item})
+  end
+end
---
+++ apps/fungifarm_shared/test/pulletmq_test.exs
@@ -0,0 +1,82 @@
+defmodule PulletMQTest do
+  use ExUnit.Case
+  doctest PulletMQ
+
+  def mktemp() do
+    tmp_dir = :os.cmd('mktemp -d') |> List.to_string() |> String.trim() |> String.to_charlist()
+
+    on_exit(fn ->
+      with {:ok, files} <- File.ls(tmp_dir) do
+        for file <- files, do: File.rm(Path.join(tmp_dir, file))
+      end
+
+      :ok = File.rmdir(tmp_dir)
+    end)
+
+    tmp_dir
+  end
+
+  setup do
+    queue_id = "queue_#{System.unique_integer([:positive, :monotonic])}" |> String.to_atom()
+
+    {:ok, pullet} = PulletMQ.start_link(data_dir: mktemp(), queue_id: queue_id)
+    {:ok, pullet: pullet}
+  end
+
+  test "more than one instance can be started" do
+    assert {:ok, pullet} = PulletMQ.start_link(data_dir: mktemp(), queue_id: :test_multiple_q1)
+    assert {:ok, pullet} = PulletMQ.start_link(data_dir: mktemp(), queue_id: :test_multiple_q2)
+  end
+
+  test "can't create more then one instance with tha same id" do
+    assert {:ok, pullet} = PulletMQ.start_link(data_dir: mktemp(), queue_id: :test_no_multiple)
+    assert {:error, {:already_started, _pid}} = PulletMQ.start_link(data_dir: mktemp(), queue_id: :test_no_multiple)
+  end
+
+  test "can push", %{pullet: pullet} do
+    assert :ok = PulletMQ.push(pullet, :item)
+  end
+
+  test "can send existing item after request", %{pullet: pullet} do
+    PulletMQ.push(pullet, :test_item)
+    PulletMQ.request(pullet, self())
+
+    assert_receive {:item, :test_item}
+  end
+
+  test "can send items pushed after request", %{pullet: pullet} do
+    PulletMQ.request(pullet, self())
+    PulletMQ.push(pullet, :test_item)
+
+    assert_receive {:item, :test_item}
+  end
+
+  test "can handle multiple requests in the right order", %{pullet: pullet} do
+    PulletMQ.request(pullet, self())
+    PulletMQ.request(pullet, self())
+
+    PulletMQ.push(pullet, :test_item1)
+    PulletMQ.push(pullet, :test_item2)
+    PulletMQ.push(pullet, :test_item3)
+
+    assert_receive {:item, :test_item1}
+    assert_receive {:item, :test_item2}
+
+    PulletMQ.request(pullet, self())
+
+    assert_receive {:item, :test_item3}
+  end
+
+  test "can be restored" do
+    dir = mktemp()
+
+    {:ok, pullet} = PulletMQ.start_link(data_dir: dir, queue_id: :test_restore)
+    PulletMQ.push(pullet, :item)
+    :ok = PulletMQ.stop(pullet)
+
+    {:ok, pullet} = PulletMQ.start_link(data_dir: dir, queue_id: :test_restore)
+    PulletMQ.request(pullet, self())
+
+    assert_receive {:item, :item}
+  end
+end
